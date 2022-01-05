d73429214b055effaee3b0bf0e02e8583c2a2106
MaksimFusion <maksim.foundation@gmail.com>
2022-01-05 16:43:35 +0300

    second commit

--- js/BattleScene.js
+++ js/BattleScene.js
@@ -1,7 +1,6 @@
 class BattleScene extends Scene {
     playerTurn = true;
     status = null;
-
     removeEventListeners = [];
 
     init() {
--- js/Battlefield.js
+++ js/Battlefield.js
@@ -206,16 +206,19 @@ class Battlefield {
                     const shot = this.shots.find(
                         (shot) => shot.x === cx && shot.y === cy);
                     shot.setVariant("killed");
-                }
-                for (let y = ship.y - 1; y < ship.y + ship.size * dy + dx + 1; y++) {
-                    for (let x = ship.x - 1; x < ship.x + ship.size * dx + dy + 1; x++) {
-                        if (this.infield(x, y)) {
-                            const item = matrix[y][x];
-                            item.free = false;
+
+                    for (let y = ship.y - 1; y < ship.y + ship.size * dy + dx + 1; y++) {
+                        for (let x = ship.x - 1; x < ship.x + ship.size * dx + dy + 1; x++) {
+                            if (this.infield(x, y)) {
+                            const shot = matrix[y][x];
+                            this.shots.push(shot);
+                            }
+                    
                         }
                     }
-                }
-            }
+
+                }}
+
         }
         this.#changed = true;
         return true;
